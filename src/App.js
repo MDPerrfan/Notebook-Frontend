e23f4766f1902f598977f1c4c26f7b30948c93b2
Mohammed Parves <mdperrfan@gmail.com>
2024-05-23 12:03:58 +0600

    Primary components has been initialized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Navbar from './components/Navbar'; 
+import Home from './components/Home'; 
+import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-<h1>HI</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
