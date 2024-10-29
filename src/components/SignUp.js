c093f5f7c6906271cd2d1618196e107fca25a986
Mohammed Parves <89094860+MDPerrfan@users.noreply.github.com>
2024-10-29 07:30:26 +0600

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -18,7 +18,7 @@ export default function SignUp(props) {
         }
 
         try {
-            const response = await fetch("http://localhost:5000/api/auth/createuser", {
+            const response = await fetch("https://notebook-backend-dn86.onrender.com/api/auth/createuser", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
