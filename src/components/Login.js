13f40ad1ef5e2f56eaec0d3c0bc836ad84914cd8
Mohammed Parves <89094860+MDPerrfan@users.noreply.github.com>
2024-10-29 07:30:00 +0600

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,7 +15,7 @@ const Login = (props) => {
     }
 
     try {
-        const response = await fetch("http://127.0.0.1:5000/api/auth/login", {
+        const response = await fetch("https://notebook-backend-dn86.onrender.com/api/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
